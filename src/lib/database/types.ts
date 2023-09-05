0d5649d0e9d7b59987f085513bc77574ed886880
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-05 00:47:28 -0700

    Creator interface

---
+++ src/lib/database/types.ts
@@ -0,0 +1,200 @@
+export enum Collections {
+  Servers = 'servers',
+  Users = 'users'
+}
+
+// Alias types for improved usability
+export type IsoDateString = string;
+export type RecordIdString = string;
+export type HTMLString = string;
+
+// System fields
+export type BaseSystemFields<T = never> = {
+  id: RecordIdString;
+  created: IsoDateString;
+  updated: IsoDateString;
+  collectionId: string;
+  collectionName: Collections;
+  expand?: T;
+};
+
+export type AuthSystemFields<T = never> = {
+  email: string;
+  emailVisibility: boolean;
+  username: string;
+  verified: boolean;
+} & BaseSystemFields<T>;
+
+export type UsersRecord = {
+  name?: string;
+  avatar?: string;
+};
+
+export type ServersRecord = {
+  title?: string;
+  icon?: string;
+  subdomain?: string;
+  serverSoftware?: ServerSoftware;
+  serverSoftwareVersion?: string;
+  gameVersion?: string;
+  worldType?: WorldType;
+  timeToLive?: TimeToLive;
+  deletionDate?: IsoDateString;
+  shutdownDate?: IsoDateString;
+  shutdown?: boolean;
+  canBeDeleted?: boolean;
+  serverFilesZiped?: string;
+};
+
+// Response types include system fields and match responses from the PocketBase API
+export type ServersResponse<Texpand = unknown> = Required<ServersRecord> & BaseSystemFields<Texpand>;
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>;
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+  servers: ServersRecord;
+  users: UsersRecord;
+};
+
+export type CollectionResponses = {
+  servers: ServersResponse;
+  users: UsersResponse;
+};
+
+export enum TimeToLive {
+  '12 hr Inactivity' = '12_hr_inactivity',
+  '1 Day' = '1_day',
+  '7 Days' = '7_days'
+}
+
+export enum ServerSoftware {
+  'Vanilla' = 'vanilla'
+  // 'Forge' = 'forge',
+  // 'Fabric' = 'fabric',
+  // 'Quilt' = 'quilt',
+  // 'Spigot' = 'spigot',
+  // 'Paper' = 'paper',
+  // 'Custom' = 'custom'
+}
+
+export const ServerSoftwareOptions: {
+  [key in ServerSoftware]?: {
+    newWorld: boolean;
+    fromSource: boolean;
+    modsUpload: boolean;
+    pluginsUpload: boolean;
+    versions: string[][];
+  };
+} = {
+  [ServerSoftware.Vanilla]: {
+    newWorld: true,
+    fromSource: true,
+    modsUpload: false,
+    pluginsUpload: false,
+    versions: [
+      ['1.8', '1.8.1', '1.8.2', '1.8.3', '1.8.4', '1.8.5', '1.8.6', '1.8.7', '1.8.8', '1.8.9'],
+      ['1.9', '1.9.1', '1.9.2', '1.9.3', '1.9.4'],
+      ['1.10', '1.10.1', '1.10.2'],
+      ['1.11', '1.11.1', '1.11.2'],
+      ['1.12', '1.12.1', '1.12.2'],
+      ['1.13', '1.13.1', '1.13.2'],
+      ['1.14', '1.14.1', '1.14.2', '1.14.3', '1.14.4'],
+      ['1.15', '1.15.1', '1.15.2'],
+      ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5'],
+      ['1.17', '1.17.1'],
+      ['1.18', '1.18.1', '1.18.2'],
+      ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+      ['1.20', '1.20.1']
+    ]
+  }
+  // [ServerSoftware.Forge]: {
+  //   newWorld: true,
+  //   fromSource: false,
+  //   modsUpload: true,
+  //   pluginsUpload: false,
+  //   versions: [
+  //     ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5'],
+  //     ['1.17', '1.17.1'],
+  //     ['1.18', '1.18.1', '1.18.2'],
+  //     ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+  //     ['1.20', '1.20.1']
+  //   ]
+  // },
+  // [ServerSoftware.Fabric]: {
+  //   newWorld: true,
+  //   fromSource: false,
+  //   modsUpload: true,
+  //   pluginsUpload: false,
+  //   versions: [
+  //     ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5'],
+  //     ['1.17', '1.17.1'],
+  //     ['1.18', '1.18.1', '1.18.2'],
+  //     ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+  //     ['1.20', '1.20.1']
+  //   ]
+  // },
+  // [ServerSoftware.Quilt]: {
+  //   newWorld: true,
+  //   fromSource: false,
+  //   modsUpload: true,
+  //   pluginsUpload: false,
+  //   versions: [
+  //     ['1.18', '1.18.1', '1.18.2'],
+  //     ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+  //     ['1.20', '1.20.1']
+  //   ]
+  // },
+  // [ServerSoftware.Spigot]: {
+  //   newWorld: true,
+  //   fromSource: false,
+  //   modsUpload: false,
+  //   pluginsUpload: true,
+  //   versions: [
+  //     ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5'],
+  //     ['1.17', '1.17.1'],
+  //     ['1.18', '1.18.1', '1.18.2'],
+  //     ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+  //     ['1.20', '1.20.1']
+  //   ]
+  // },
+  // [ServerSoftware.Paper]: {
+  //   newWorld: true,
+  //   fromSource: false,
+  //   modsUpload: false,
+  //   pluginsUpload: true,
+  //   versions: [
+  //     ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5'],
+  //     ['1.17', '1.17.1'],
+  //     ['1.18', '1.18.1', '1.18.2'],
+  //     ['1.19', '1.19.1', '1.19.2', '1.19.3', '1.19.4'],
+  //     ['1.20', '1.20.1']
+  //   ]
+  // }
+};
+
+export enum WorldCreationMethod {
+  'New' = 'new',
+  'Upload' = 'upload'
+}
+
+export enum WorldType {
+  'Normal' = 'normal',
+  'Large Biomes' = 'large biomes',
+  'Superflat' = 'superflat',
+  'Amplified' = 'amplified'
+}
+
+export enum Difficulty {
+  'Peaceful' = 'peaceful',
+  'Easy' = 'easy',
+  'Normal' = 'normal',
+  'Hard' = 'hard'
+}
+
+export enum Gamemode {
+  'Survival' = 'survival',
+  'Creative' = 'creative',
+  'Adventure' = 'adventure',
+  'Spectator' = 'spectator'
+}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,19 @@
-// place files you want to import through the `$lib` alias in this folder.
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+export function timeUntil(date: Date | string | number) {
+  return dayjs(date).fromNow();
+}
+
+export function formDataObject(formData: FormData) {
+  const object: Record<string, unknown> = {};
+  for (const [key, value] of formData.entries()) {
+    if (value instanceof File && value.size === 0) continue;
+    if (object[key] !== undefined) {
+      if (!Array.isArray(object[key])) object[key] = [object[key]];
+      object[key].push(value);
+    } else object[key] = value;
+  }
+  return object;
+}
