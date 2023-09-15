fa79fb3d2c0b8ae6a401c0ed1ede8de60946d1ef
melonboy10 <aidansocal844@gmail.com>
2023-09-15 04:00:00 -0700

    Fix tailwind colors, :facepalm:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors: {
-      primary: '#ffd392',
-      secondary: '#e6bc8a',
-      tertiary: '#aa9d8e'
+    extend: {
+      colors: {
+        primary: '#ffd392',
+        secondary: '#e6bc8a',
+        tertiary: '#aa9d8e'
+      }
     }
   },
   plugins: ['prettier-plugin-tailwindcss']
