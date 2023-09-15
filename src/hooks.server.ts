65a01893f533ef8cb32ca932e1370f95d4326772
melonboy10 <aidansocal844@gmail.com>
2023-09-15 05:40:35 -0700

    Get environment ready

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,13 @@
 // src/hooks.server.js
-import { POCKETBASE_INTERNAL_ADMIN_EMAIL, POCKETBASE_INTERNAL_ADMIN_PASSWORD } from '$env/static/private';
+import { env } from '$env/dynamic/private';
+import { Collections } from '$lib/database/types';
 import PocketBase from 'pocketbase';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   event.locals.pb = new PocketBase('http://127.0.0.1:8090');
   event.locals.pb.autoCancellation(false);
-  event.locals.pb.admins.authWithPassword(POCKETBASE_INTERNAL_ADMIN_EMAIL, POCKETBASE_INTERNAL_ADMIN_PASSWORD);
+  event.locals.pb.collection(Collections.Users).authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
 
   const response = await resolve(event);
   return response;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import '../app.css';
+  import { version } from '$app/environment';
 </script>
 
 <div class="flex h-full w-full flex-col items-center gap-4 p-8">
@@ -8,7 +9,10 @@
       <img src="/wing.png" alt="Logo" class="h-10 w-10" />
       <a href="/" class="text-3xl font-black">Hermes</a>
     </div>
-    <a href="/create" class="bg-tertiary rounded-md p-2 px-4 text-lg font-bold">Create</a>
+    <a href="/create" class="rounded-md bg-tertiary p-2 px-4 text-lg font-bold">Create</a>
   </div>
   <slot />
+  <p>
+    Version: {version}
+  </p>
 </div>
