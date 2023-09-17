355a13231a9026f35fe1d0f92fb776f5b2c0718b
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 02:40:07 -0700

    String version

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
 // src/hooks.server.js
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 import PocketBase from 'pocketbase';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  event.locals.pb = new PocketBase('http://0.0.0.0:8090');
+  event.locals.pb = new PocketBase(dev ? 'http://127.0.0.1:8090' : 'http://0.0.0.0:8090');
   event.locals.pb.autoCancellation(false);
   try {
     event.locals.pb.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
