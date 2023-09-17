cbea2fec8ac7a17c14c638e1e131a5edf9081e3e
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 03:06:10 -0700

    Check if variables are set

--- database/pb_migrations/1694942616_add_default_account.js
+++ database/pb_migrations/1694942616_add_default_account.js
@@ -5,6 +5,8 @@ migrate((db) => {
   const adminEmail = $os.getenv('POCKETBASE_INTERNAL_ADMIN_EMAIL');
   const adminPassword = $os.getenv('POCKETBASE_INTERNAL_ADMIN_PASSWORD');
 
+  console.log(`Creating admin account with email ${adminEmail}`);
+
   const admin = new Admin();
   admin.email = adminEmail;
   admin.setPassword(adminPassword);
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
   import '../app.css';
-  import { version } from '/package.json';
 </script>
 
 <div class="flex h-full w-full flex-col items-center gap-4 p-8">
@@ -12,7 +11,5 @@
     <a href="/create" class="rounded-md bg-tertiary p-2 px-4 text-lg font-bold">Create</a>
   </div>
   <slot />
-  <p>
-    Version: {version}
-  </p>
+  <p>Version: 0.5.0</p>
 </div>
