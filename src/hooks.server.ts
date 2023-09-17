cf923c276763149d73962d6b6b3324040602944d
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 02:29:00 -0700

    Remove boot hood and make a migration

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,16 @@
 // src/hooks.server.js
 import { env } from '$env/dynamic/private';
-import { Collections } from '$lib/database/types';
 import PocketBase from 'pocketbase';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   event.locals.pb = new PocketBase('http://127.0.0.1:8090');
   event.locals.pb.autoCancellation(false);
-  event.locals.pb.collection(Collections.Users).authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
+  try {
+    event.locals.pb.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
+  } catch (e) {
+    console.error('Failed to authenticate with PocketBase (thats not good)');
+  }
 
   const response = await resolve(event);
   return response;
