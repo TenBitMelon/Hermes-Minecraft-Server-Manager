de4d83472a4d6f999bf5e972cd018fd5565a555b
melonboy10 <aidansocal844@gmail.com>
2023-09-17 02:29:00 -0700

    Remove boot hood and make a migration

---
+++ database/pb_migrations/1694942616_add_default_account.js
@@ -0,0 +1,13 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db);
+
+  const adminEmail = $os.getenv('POCKETBASE_INTERNAL_ADMIN_EMAIL');
+  const adminPassword = $os.getenv('POCKETBASE_INTERNAL_ADMIN_PASSWORD');
+
+  const admin = new Admin();
+  admin.email = adminEmail;
+  admin.setPassword(adminPassword);
+
+  dao.saveAdmin(admin);
+});
