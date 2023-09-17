9b9afc20bc8e287cd448ca4ff4325d52b2c70fd6
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 04:24:19 -0700

    figulate

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,8 +5,7 @@ import PocketBase from 'pocketbase';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  event.locals.pb = new PocketBase('http://127.0.0.1:8090');
-  // event.locals.pb = new PocketBase(dev ? 'http://127.0.0.1:8090' : 'http://0.0.0.0:8090');
+  event.locals.pb = new PocketBase(dev ? 'http://127.0.0.1:8090' : 'http://0.0.0.0:8090');
   event.locals.pb.autoCancellation(false);
   try {
     console.log(`Authenticating with PocketBase using email: ${env.POCKETBASE_INTERNAL_ADMIN_EMAIL} password: ${env.POCKETBASE_INTERNAL_ADMIN_PASSWORD}`);
