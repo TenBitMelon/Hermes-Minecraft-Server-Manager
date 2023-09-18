bc62bd440715e19d36c81e75dfb1ba736e9c1cb7
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 23:05:54 -0700

    catch logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,10 +9,10 @@ export async function handle({ event, resolve }) {
   event.locals.pb.autoCancellation(false);
   try {
     console.log(`Authenticating with PocketBase using email: ${env.POCKETBASE_INTERNAL_ADMIN_EMAIL} password: ${env.POCKETBASE_INTERNAL_ADMIN_PASSWORD}`);
-    console.log('fake', await (await fetch('http://mmsdmfmsdmfmsdmf.mdmd')).json());
-    console.log('localhost', await (await fetch('http://localhost:8090/api')).json());
-    console.log('127', await (await fetch('http://127.0.0.1:8090/api')).json());
-    console.log('0', await (await fetch('http://0.0.0.0:8090/api')).json());
+    console.log('fake', await (await fetch('http://mmsdmfmsdmfmsdmf.mdmd')).json().catch(() => 'failed'));
+    console.log('localhost', await (await fetch('http://localhost:8090/api')).json().catch(() => 'failed'));
+    console.log('127', await (await fetch('http://127.0.0.1:8090/api')).json().catch(() => 'failed'));
+    console.log('0', await (await fetch('http://0.0.0.0:8090/api')).json().catch(() => 'failed'));
     event.locals.pb.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
     console.log('Authenticated with PocketBase');
   } catch (e) {
