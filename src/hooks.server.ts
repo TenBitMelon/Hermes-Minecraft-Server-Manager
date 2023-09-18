1c5f1afaafc8a91fa76e1b195266ebf28818c227
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-17 22:38:00 -0700

    more logginb

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,7 +9,9 @@ export async function handle({ event, resolve }) {
   event.locals.pb.autoCancellation(false);
   try {
     console.log(`Authenticating with PocketBase using email: ${env.POCKETBASE_INTERNAL_ADMIN_EMAIL} password: ${env.POCKETBASE_INTERNAL_ADMIN_PASSWORD}`);
+    console.log(await (await fetch('http://0.0.0.0:8090/api')).json());
     event.locals.pb.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
+    console.log('Authenticated with PocketBase');
   } catch (e) {
     console.log('Failed to authenticate with PocketBase (thats not good)');
   }
