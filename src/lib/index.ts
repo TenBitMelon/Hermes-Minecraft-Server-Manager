6d70406ca8910d1434f779d01b69ff66611f24f0
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2023-09-19 03:29:15 -0700

    else

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,14 +7,17 @@ import { env } from '$env/dynamic/private';
 import { dev, building } from '$app/environment';
 import PocketBase from 'pocketbase';
 
-const serverPB = new PocketBase(!building ? (dev ? 'http://127.0.0.1:8090' : 'http://0.0.0.0:8090') : '');
-serverPB.autoCancellation(false);
-try {
-  console.log(`Authenticating with PocketBase using email: ${env.POCKETBASE_INTERNAL_ADMIN_EMAIL} password: ${env.POCKETBASE_INTERNAL_ADMIN_PASSWORD}`);
-  serverPB.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
-  console.log('Authenticated with PocketBase');
-} catch (e) {
-  console.log('Failed to authenticate with PocketBase (thats not good)');
+let serverPB;
+if (!building) {
+  serverPB = new PocketBase(dev ? 'http://127.0.0.1:8090' : 'http://0.0.0.0:8090');
+  serverPB.autoCancellation(false);
+  try {
+    console.log(`Authenticating with PocketBase using email: ${env.POCKETBASE_INTERNAL_ADMIN_EMAIL} password: ${env.POCKETBASE_INTERNAL_ADMIN_PASSWORD}`);
+    serverPB.admins.authWithPassword(env.POCKETBASE_INTERNAL_ADMIN_EMAIL, env.POCKETBASE_INTERNAL_ADMIN_PASSWORD);
+    console.log('Authenticated with PocketBase');
+  } catch (e) {
+    console.log('Failed to authenticate with PocketBase (thats not good)');
+  }
 }
 export { serverPB };
 
