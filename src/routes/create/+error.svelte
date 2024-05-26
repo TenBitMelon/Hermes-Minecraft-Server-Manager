af0ecabd66d56bbb5f9bbd4fb54716ae4b412bcf
TenBitMelon <56051776+TenBitMelon@users.noreply.github.com>
2024-05-26 05:45:02 -0700

    Added no available port errors

---
+++ src/routes/create/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center gap-5 py-20">
+  <div class="text-lg text-gray-200">Sorry. You cannot create a server at this time.</div>
+  <a href="/" class="rounded-md border-2 border-primary bg-transparent p-2 px-4 text-lg font-bold text-primary transition-colors hover:bg-primary hover:text-black">Go back to Home</a>
+</div>
